5a23629dd09f2bae7af4ad69b037db0a7d01f6d5
Louis <louis.zerri@hotmail.com>
2017-04-13 03:43:36 +0200

    Ajout du fichier Ennemi.cs

---
+++ Ennemi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ennemi : Unit
+{
+    public Ennemi()
+    {
+        this.nom = "Bidule";
+        this.points_de_vie = 100;
+        this.points_armure = 20;
+    }
+
+    public Ennemi(String name, int pv, int pa)
+    {
+        this.nom = name;
+        this.points_de_vie = pv;
+        this.points_armure = pa;
+    }
+}
